41ae2cb9f743dbd2d2b416f42db37fa006b23254
Diego Fernando Arizala Revelo <dfarizala@gmail.com>
2022-05-03 14:58:09 -0500

    Publication Services

--- MainsoftTesting.Application/Exam/Operations.cs
+++ MainsoftTesting.Application/Exam/Operations.cs
@@ -7,6 +7,7 @@ using MainsoftTesting.Domain.Models;
 using MainsoftTesting.Infrastructure.Users;
 using MainsoftTesting.Domain.Models.CQRS.Request;
 using MainsoftTesting.Domain.Models.CQRS.Response;
+using MainsoftTesting.Domain.CQRS.Request;
 
 namespace MainsoftTesting.Application.Exam
 {
@@ -17,6 +18,9 @@ namespace MainsoftTesting.Application.Exam
 
         public static Task<GetAssignedExamsResponse> GetAssignedExam()
             => Infrastructure.Exams.Operations.GetAssignedExams();
+
+        public static Task<AssignExamResponse> AssignExam(AssignExamRequest request)
+            => Infrastructure.Exams.Operations.AssignExam(request);
 
     }
 }
